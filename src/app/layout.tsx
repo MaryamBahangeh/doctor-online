19e030919549832f73cf1de1d8f68b37af44b423
Maryam <maryam.bahangeh@gmail.com>
2024-12-20 11:56:55 +0100

    [ADD] preitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Open_Sans } from 'next/font/google'
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
 const openSans = Open_Sans({
-    subsets: ['latin'],
-    display: 'swap',
-})
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Doctor Online",
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={openSans.className} >
+    <html lang="en" className={openSans.className}>
       <body>{children}</body>
     </html>
   );
