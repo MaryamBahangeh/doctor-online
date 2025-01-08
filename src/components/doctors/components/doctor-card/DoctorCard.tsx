eb114874e646e33f536a3d9cc0daf9c6934b6251
Maryam <maryam.bahangeh@gmail.com>
2025-01-08 18:08:40 +0100

    [UPDATE] Doctor card

--- src/components/doctors/components/doctor-card/DoctorCard.tsx
+++ src/components/doctors/components/doctor-card/DoctorCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import StarBold from "@/icons/StarBold";
 import { DoctorModel } from "@/models/doctor";
 import Card from "@/components/card/Card";
 import Image from "next/image";
 import styles from "./DoctorCard.module.css";
+import MingcuteLocationLine from "@/icons/MingcuteLocationLine";
 
 type Props = {
   doctor: DoctorModel;
@@ -12,14 +14,46 @@ function DoctorCard({ doctor }: Props) {
   return (
     <Card>
       <div className={styles["doctor-card"]}>
-        <Image src={doctor.image} alt={"doctor-image"} width={20} height={20} />
-        <div>{doctor.name}</div>
-        <div>{doctor.specialityName}</div>
-        <div>{doctor.address}</div>
-        <div>{doctor.rate}</div>
-        <div>{doctor.totalVotes}</div>
-        <div>{doctor.serviceType}</div>
-        <div>{doctor.firstAvailableAppointment}</div>
+        <div className={styles["container"]}>
+          <div className={styles["image-container"]}>
+            <Image
+              className={styles.image}
+              src={doctor.image}
+              alt="doctor image"
+            ></Image>
+          </div>
+
+          <div className={styles["doctor-info"]}>
+            <div className={styles.name}>{doctor.name}</div>
+            <div className={styles.description}>{doctor.specialityName}</div>
+            <div className={styles.rate}>
+              <StarBold color={"gold"} /> <span> {doctor.rate}</span>
+              {" (" + doctor.totalVotes + " comments)"}
+            </div>
+          </div>
+        </div>
+        <div className={styles["other-info"]}>
+          <div>{doctor.description}</div>
+          <div className={styles.address}>
+            <MingcuteLocationLine />
+            {doctor.address}
+          </div>
+
+          <div>
+            Service type :{" "}
+            <span className={styles.data}>{doctor.serviceType}</span>
+          </div>
+
+          <div>
+            First available appointment :
+            <span className={styles.data}>
+              {doctor.firstAvailableAppointment}
+            </span>
+          </div>
+          <div className={styles.book}>
+            <button>Book an online appointment</button>
+          </div>
+        </div>
       </div>
     </Card>
   );
--- src/models/doctor.ts
+++ src/models/doctor.ts
@@ -1,14 +1,16 @@
 import { ServiceType } from "@/enums/service-type";
+import Image from "next/image";
 
 export type DoctorModel = {
   id: string;
   name: string;
   serviceType: ServiceType;
-  image: string;
+  image: typeof Image;
   rate: number;
   totalVotes: number;
   address: string;
   firstAvailableAppointment: string;
   specialityId: string;
   specialityName: string;
+  description: string;
 };
