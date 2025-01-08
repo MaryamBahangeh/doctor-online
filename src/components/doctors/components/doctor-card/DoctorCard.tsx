9b6b6b5780b5ee53e30a8c7cba7eea143cc8b533
Maryam <maryam.bahangeh@gmail.com>
2025-01-08 21:01:04 +0100

    [REFACTOR] refactor

--- src/components/doctors/components/doctor-card/DoctorCard.tsx
+++ src/components/doctors/components/doctor-card/DoctorCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-
 import Card from "@/components/card/Card";
 import StarBold from "@/icons/StarBold";
 import MingcuteLocationLine from "@/icons/MingcuteLocationLine";
