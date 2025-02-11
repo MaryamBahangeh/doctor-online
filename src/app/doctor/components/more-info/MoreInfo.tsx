db2df19688e3c46cd5b031c85ad719fbc214a502
Maryam <maryam.bahangeh@gmail.com>
2025-02-11 19:09:15 +0100

    [UPDATE] more-info component

--- src/app/doctor/components/more-info/MoreInfo.tsx
+++ src/app/doctor/components/more-info/MoreInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./MoreInfo.module.css";
 import Card from "@/components/card/Card";
 import { DoctorModel } from "@/models/doctor";
+import Button, { Color, Variant } from "@/components/Button/Button";
 
 type Props = {
   doctor: DoctorModel;
@@ -10,15 +11,33 @@ type Props = {
 function MoreInfo({ doctor }: Props) {
   return (
     <Card className={styles["more-info"]}>
-      <div className={styles.amount}>${doctor.amount}</div>
-      <div>
-        An online meeting for <span>30 minutes</span> video call
-      </div>
-      <div>
-        You can ask the doctor any questions you have for up to
-        <span> 3 days</span>
-      </div>
-      <button>Book an appointment now</button>
+      <header>
+        <div className={styles.indicator}></div>
+        <div className={styles.title}>Book an appointment now!</div>
+        <div className={styles.price}>${doctor.price}</div>
+      </header>
+
+      <main>
+        <ul className={styles.perks}>
+          <li className={styles.perk}>
+            An online meeting for <span>30 minutes</span> video call
+          </li>
+          <li className={styles.perk}>
+            You can ask the doctor any questions you have for up to
+            <span> 3 days</span>
+          </li>
+        </ul>
+      </main>
+
+      <footer>
+        <Button
+          className={styles.appointment}
+          variant={Variant.SOLID}
+          color={Color.PRIMARY}
+        >
+          Book an appointment now
+        </Button>
+      </footer>
     </Card>
   );
 }
