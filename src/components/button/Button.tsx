115214e0293ce656e27dabfa77537859988b171c
Maryam.bahangeh <maryam.bahangeh@gmail.com>
2025-03-05 20:24:54 +0100

    [UPDATE]fix button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,69 @@
+import { ComponentProps, ReactElement } from "react";
+
+import clsx from "clsx";
+
+import styles from "./Button.module.css";
+import Link from "next/link";
+
+export enum ButtonType {
+  LINK = "link",
+  BUTTON = "button",
+}
+
+export enum Color {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+  DANGER = "danger",
+  IDLE = "idle",
+}
+
+export enum Variant {
+  SOLID = "solid",
+  OUTLINE = "outline",
+  TEXT = "text",
+}
+
+type Props = ComponentProps<"button"> & {
+  buttonType?: ButtonType;
+  linkTo?: string;
+  variant: Variant;
+  color?: Color;
+};
+
+function Button({
+  buttonType = ButtonType.BUTTON,
+  linkTo,
+  variant,
+  color,
+  className,
+  children,
+  ...rest
+}: Props): ReactElement {
+  return buttonType === ButtonType.BUTTON ? (
+    <button
+      className={clsx(
+        styles.general,
+        styles[variant],
+        color && styles[color],
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  ) : (
+    <Link
+      href={linkTo!}
+      className={clsx(
+        styles.general,
+        styles[variant],
+        color && styles[color],
+        className,
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default Button;
