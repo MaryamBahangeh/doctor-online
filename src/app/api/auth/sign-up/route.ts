b09b8e0daa2da68c7d5c242475a1102b6cf698a0
Maryam.bahangeh <maryam.bahangeh@gmail.com>
2025-03-09 19:50:55 +0100

    [ADD] parseBody

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -2,14 +2,18 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/lib/prisma";
 
-import { wrapWithTryCatch } from "@/utils/api.utils";
+import { parseBody, wrapWithTryCatch } from "@/utils/api.utils";
 
 import { ApiResponseType } from "@/types/api-response.type";
+import { SignUpDto } from "@/dto/auth.dto";
 
 export async function POST(request: Request): Promise<ApiResponseType<null>> {
   return wrapWithTryCatch(async () => {
-    const body = await request.json();
+    const [parseError, body] = await parseBody<SignUpDto>(request);
 
+    if (parseError !== null) {
+      return NextResponse.json({ error: parseError, status: 400 });
+    }
     await prisma.user.create({ data: body });
 
     return NextResponse.json({ data: null }, { status: 201 });
--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,6 +1,31 @@
 import { NextResponse } from "next/server";
 import { ApiResponseType } from "@/types/api-response.type";
 
+type ParseBodyResult<T> = [error: null, data: T] | [error: string, data: null];
+
+export async function parseBody<T>(
+  request: Request,
+): Promise<ParseBodyResult<T>> {
+  try {
+    const body = await request.json();
+    return [null, body];
+  } catch (error) {
+    if (typeof error === "string") {
+      return [error, null];
+    }
+
+    if (error instanceof Error) {
+      if (error.name === "SyntaxError") {
+        return ["Boy data format is incorrect", null];
+      }
+
+      return [error.message, null];
+    }
+
+    return ["An unexpected error occurred", null];
+  }
+}
+
 export async function wrapWithTryCatch<T>(
   callback: () => Promise<ApiResponseType<T>>,
 ): Promise<ApiResponseType<T>> {
