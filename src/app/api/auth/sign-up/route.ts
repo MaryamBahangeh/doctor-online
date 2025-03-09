b9685898da8b16f8be90c07a0210a056f1e4752c
Maryam.bahangeh <maryam.bahangeh@gmail.com>
2025-03-09 20:04:53 +0100

    [ADD] username and email validation

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -14,6 +14,29 @@ export async function POST(request: Request): Promise<ApiResponseType<null>> {
     if (parseError !== null) {
       return NextResponse.json({ error: parseError, status: 400 });
     }
+
+    let foundUser = await prisma.user.findUnique({
+      where: { username: body.username },
+    });
+
+    if (foundUser) {
+      return NextResponse.json({
+        error: "This username is already taken",
+        status: 400,
+      });
+    }
+
+    foundUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    if (foundUser) {
+      return NextResponse.json({
+        error: "This email is already taken",
+        status: 400,
+      });
+    }
+
     await prisma.user.create({ data: body });
 
     return NextResponse.json({ data: null }, { status: 201 });
